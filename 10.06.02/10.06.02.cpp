59411fa0491973d627325ae8e1eaee61e88f81fe
Admin <Admin@WIN-5I7J1UL4AE7>
2023-06-10 21:13:42 +0300

    Добавьте файлы проекта.

---
+++ 10.06.02/10.06.02.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+#include<array>
+using namespace std;
+
+int main()
+{
+    array<int, 10> masuv {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n;
+    int найбiльший = masuv[0];
+    int найменший = masuv[0];
+    for (int i = 0;i < masuv.size();i++) {
+        if(masuv.at(i) > найбiльший){
+            найбiльший = masuv.at(i);
+ 
+        }
+        if (masuv.at(i) < найменший) {
+            найменший = masuv.at(i);
+        }
+    }
+    cout << найбiльший << endl;
+    cout << найменший;
+}
+
